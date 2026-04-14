cb6b6711125ead25fefc3dd55e1195464c6d26c6
Yous237 <salouhouyousuf@gmail.com>
2026-04-14 14:36:07 +0200

    d

--- app.js
+++ app.js
@@ -13,3 +13,14 @@ function render() {
   const list = document.getElementById('taskList');
   list.innerHTML = tasks.map(t => <li>${t}</li>).join('');
 }
+// app.js  feature/add-task
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  render();
+}
+function render() {
+  const list = document.getElementById('taskList');
+  list.innerHTML = tasks.map((t, i) =>
+    <li>${t} <button onclick="deleteTask(${i})"> XXX </button></li>
+  ).join('');
+}
